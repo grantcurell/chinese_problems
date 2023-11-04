090305844054f93d040b01c1c8a79f5c7d387538
Grant Curell <grantcurell@gmail.com>
2023-11-04 18:35:01 -0400

    Initial vectors

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import requests
 import json
-from gensim.models import Word2Vec
+from gensim.models import KeyedVectors
 from collections import Counter
 import jieba
 import csv
-from gensim.models import KeyedVectors
+
 
 def invoke(action, **params):
     request_json = json.dumps({
@@ -15,40 +15,78 @@ def invoke(action, **params):
     response = requests.post('http://localhost:8765', data=request_json)
     return json.loads(response.content)
 
+
 def get_words_to_review(deck_name):
     query = f'"note:Chinese Words Hanzicraft" (is:review OR is:learn)'
     result = invoke('findNotes', query=query)
     note_ids = result.get('result', [])
     note_info = invoke('notesInfo', notes=note_ids)
-    return [note['fields']['Traditional']['value'] for note in note_info['result']] if 'result' in note_info else []
 
-def extract_features(words, model_path):
-    model = KeyedVectors.load_word2vec_format(model_path, binary=False)  # Load your pre-trained Word2Vec model in the correct format
+    words_with_lapses = []
+    for note in note_info['result']:
+        note_fields = note['fields']
+        traditional_word = note_fields['Traditional']['value']
+        simplified_word = note_fields['Simplified']['value'] if note_fields['Simplified']['value'] else None
+        # Extracting cards associated with each note
+        cards = note['cards']
+        cards_info = invoke('cardsInfo', cards=cards)
+        lapses = sum(card['lapses'] for card in cards_info['result'])
+
+        # Add simplified word to the tuple if it exists
+        word_info = (traditional_word, lapses)
+        if simplified_word:
+            word_info += (simplified_word,)
+
+        words_with_lapses.append(word_info)
+
+    return words_with_lapses
+
+
+def extract_features(words_with_lapses, model_path):
+    model = KeyedVectors.load_word2vec_format(model_path, binary=False)
     features = []
-    for word in words:
-        if word in model:  # Check if the word is in the model vocabulary
-            vec = model[word]  # Get the word vector
-            features.append(
-                {'word': word, 'vector': vec.tolist(), 'frequency': Counter(jieba.cut(word))}
-            )  # Add the word, its vector, and frequency data to the features list
+    for word_info in words_with_lapses:
+        word = word_info[0]
+        incorrect_count = word_info[1]
+        simplified_word = word_info[2] if len(word_info) > 2 else None
+        if word in model:
+            vec = model[word]
+            feature = {
+                'word': word,
+                'vector': vec.tolist(),
+                'frequency': Counter(jieba.cut(word)),
+                'incorrect_count': incorrect_count
+            }
+            if simplified_word:
+                feature['simplified_word'] = simplified_word
+            features.append(feature)
     return features
+
 
 def save_features_to_csv(features, filename):
     with open(filename, 'w', newline='', encoding='utf-8-sig') as file:
         writer = csv.writer(file)
-        # Write the header
-        writer.writerow(['word', 'vector', 'character_frequency'])
+        header = ['word', 'vector', 'character_frequency', 'incorrect_count']
+        # Check if simplified words are in the features to determine if the column should be added
+        if any('simplified_word' in feature for feature in features):
+            header.insert(1, 'simplified_word')
+        writer.writerow(header)
         for feature in features:
             word = feature['word']
             vector = feature['vector']
             frequency = feature['frequency']
-            # Flatten frequency counter to a string representation
+            incorrect_count = feature['incorrect_count']
             frequency_str = '; '.join([f'{char}:{count}' for char, count in frequency.items()])
-            writer.writerow([word, vector, frequency_str])  # Write the word, its vector, and frequency data as a row in the CSV
+            row = [word, vector, frequency_str, incorrect_count]
+            # Insert simplified word into the row if it exists
+            if 'simplified_word' in feature:
+                row.insert(1, feature['simplified_word'])
+            writer.writerow(row)
+
 
 if __name__ == '__main__':
     deck_name = "Chinese::HanziCraft Review"
     words_to_review = get_words_to_review(deck_name)
     model_path = "./sgns.merge.word"
     features_dataset = extract_features(words_to_review, model_path)
-    save_features_to_csv(features_dataset, 'features_dataset.csv')  # Save the features to a CSV file named 'features_dataset.csv'
+    save_features_to_csv(features_dataset, 'features_dataset.csv')
